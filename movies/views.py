509dec969b6ece0610f8b9e4b052db661fbd4ba7
PJ Sison <peejh.sison@gmail.com>
2024-07-05 19:27:54 -0400

    initial commit

---
+++ movies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'movies': ['gladiator', 'lord of the rings', 'dark knight']
+    }
+    return render(request, "movies/index.html", context)
+
+def about(request):
+    return render(request, "movies/about.html", {})
